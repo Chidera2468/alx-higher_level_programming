c164dd08c7a3bd12c188398d688b2fa34750d07c
Chidera2468 <chidera2468@gmail.com>
2023-01-02 20:02:24 -0800

    rough

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -64,12 +64,12 @@ class Rectangle:
                 rect.append("\n")
                 return ("".join(rect))
 
-            def __repr__(self):
-                """Return the string representation of the Rectangle."""
-                rect = "Rectangle(" + str(self.__width)
-                rect += ", " + str(self.__height) + ")"
-                return (rect)
+    def __repr__(self):
+        """Return the string representation of the Rectangle."""
+        rect = "Rectangle(" + str(self.__width)
+        rect += ", " + str(self.__height) + ")"
+        return (rect)
 
-            def __del__(self):
-                """Print a message for every deletion of a Rectangle."""
-                print("Bye rectangle...")
+    def __del__(self):
+        """Print a message for every deletion of a Rectangle."""
+        print("Bye rectangle...")
